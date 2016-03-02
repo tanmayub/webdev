58e85b12939b2c7dcdc903bb4f458fbd1e0e9afa
Tanmay Budukh <tanmayub@ccs.neu.edu>
2016-03-01 20:38:47 -0500

    Assignment 2

--- public/assignment/views/forms/forms.controller.js
+++ public/assignment/views/forms/forms.controller.js
@@ -8,6 +8,10 @@
 
     function formController($scope, $routeParams, FormService, $rootScope, $location) {
         $scope.findAllFormsForUser = findAllFormsForUser;
+        $scope.updateForm = updateForm;
+        $scope.addForm = addForm;
+        $scope.deleteForm = deleteForm;
+        $scope.selectForm = selectForm;
 
         findAllFormsForUser();
 
@@ -18,6 +22,30 @@
                 console.log(response);
                 $scope.forms = response;
             });
+        }
+
+        function addForm(title) {
+            var form = {"title": title};
+            FormService.createFormForUser($rootScope.loggedUser._id, form, function(response) {
+                console.log(response);
+            });
+        }
+
+        function updateForm(formId, title) {
+            var form = {"title": title};
+            FormService.updateFormById(formId, form, function(response) {
+                console.log(response);
+            });
+        }
+
+        function deleteForm(formId) {
+            FormService.deleteFormById(formId, function(response) {
+                console.log(response);
+            });
+        }
+
+        function selectForm() {
+
         }
     }
 })();
