7d4fe0ea356411ac77210162894afec3d1efe66e
Tanmay Budukh <tanmayub@ccs.neu.edu>
2016-04-01 19:21:17 -0400

    forms CRUD operations schema changes

--- public/assignment/server/models/form.schema.server.js
+++ public/assignment/server/models/form.schema.server.js
@@ -2,12 +2,15 @@
  * Created by TanmayPC on 3/31/2016.
  */
 module.exports = function(mongoose) {
+
+    var FieldSchema = require("./field.schema.server.js")(mongoose);
+
     var FormSchema = mongoose.Schema({
         userId: String,
         title: String,
         created: Date,
         updated: Date
-        //fields: [String]
+        //fields: [FieldSchema]
     }, {collection: 'form'});
 
     return FormSchema;
